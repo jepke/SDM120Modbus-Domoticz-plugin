6633cf304c2d6ff4446b010eef3b2f1549b70e5f
jepke <jepke@users.noreply.github.com>
2023-05-22 22:52:40 +0100

    First set of modbus values

--- plugin.py
+++ plugin.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 """
-Eastron SDM120-Modbus Smart Meter Three Phase Electrical System. The Python plugin for Domoticz
-Original author: MFxMF and bbossink
-Modified by: remcovanvugt
+Eastron SDM630-ModbusV2 Smart Meter Three Phase Electrical System. The Python plugin for Domoticz
+Original author: MFxMF,  bbossink, remocovanvugt
+Modified by: Jepke
 Requirements: 
     1.python module minimalmodbus -> http://minimalmodbus.readthedocs.io/en/master/
         (pi@raspberrypi:~$ sudo pip3 install minimalmodbus)
     2.Communication module Modbus USB to RS485 converter module
 """
 """
-<plugin key="SDM120Modbus" name="Eastron SDM120-Modbus" version="1.0.0" author="remcovanvugt">
+<plugin key="SDM630ModbusV2" name="Eastron SDM630-ModbusV2" version="2.0.0" author="Jepke">
     <params>
         <param field="SerialPort" label="Modbus Port" width="200px" required="true" default="/dev/ttyUSB0" />
         <param field="Mode1" label="Baud rate" width="40px" required="true" default="9600"  />
@@ -49,66 +49,161 @@ class BasePlugin:
 
         self.rs485.mode = minimalmodbus.MODE_RTU
         devicecreated = []
-        Domoticz.Log("Eastron SDM120 Modbus plugin start")
+        Domoticz.Log("Eastron SDM630 Modbus V2 plugin start")
         self.runInterval = int(Parameters["Mode3"]) * 1 
        
         if 1 not in Devices:
-            Domoticz.Device(Name="Total System Power", Unit=1,TypeName="Usage",Used=0).Create()
-        Options = { "Custom" : "1;VA"} 
+            Domoticz.Device(Name="Voltage_L1", Unit=1,TypeName="Voltage",Used=0).Create()
         if 2 not in Devices:
-            Domoticz.Device(Name="Import Wh", Unit=2,Type=0x71,Subtype=0x0,Used=0).Create()
-        Options = { "Custom" : "1;kVArh"}
+            Domoticz.Device(Name="Voltage_L2", Unit=2,TypeName="Voltage",Used=0).Create()
         if 3 not in Devices:
-            Domoticz.Device(Name="Export Wh", Unit=3,Type=0x71,Subtype=0x0,Used=0).Create()
-        Options = { "Custom" : "1;kVArh"} 
+            Domoticz.Device(Name="Voltage_L3", Unit=3,TypeName="Voltage",Used=0).Create()
+        Options = { "Custom" : "1;A"}
         if 4 not in Devices:
-            Domoticz.Device(Name="Total kWh", Unit=4,Type=0x71,Subtype=0x0,Used=0).Create()
-        Options = { "Custom" : "1;kVArh"}
+            Domoticz.Device(Name="Current_L1", Unit=4,TypeName="Custom",Used=0,Options=Options).Create()
         if 5 not in Devices:
-            Domoticz.Device(Name="Voltage", Unit=5,Type=0x71,Subtype=0x0,Used=0).Create()
-        Options = { "Custom" : "1;V"}
+            Domoticz.Device(Name="Current_L2", Unit=5,TypeName="Custom",Used=0,Options=Options).Create()
         if 6 not in Devices:
-            Domoticz.Device(Name="Import power", Unit=6,TypeName="Usage",Used=0).Create()
-        Options = { "Custom" : "1;VA"} 
+            Domoticz.Device(Name="Current_L3", Unit=6,TypeName="Custom",Used=0,Options=Options).Create()
         if 7 not in Devices:
-            Domoticz.Device(Name="Export power", Unit=7,TypeName="Usage",Used=0).Create()
-        Options = { "Custom" : "1;VA"} 
+            Domoticz.Device(Name="Power_L1", Unit=7,TypeName="Usage",Used=0).Create()
+        if 8 not in Devices:
+            Domoticz.Device(Name="Power_L2", Unit=8,TypeName="Usage",Used=0).Create()
+        if 9 not in Devices:
+            Domoticz.Device(Name="Power_L3", Unit=9,TypeName="Usage",Used=0).Create()
+        Options = { "Custom" : "1;VA"}
+        if 10 not in Devices:
+            Domoticz.Device(Name="Apparent_Power_L1", Unit=10,TypeName="Custom",Used=0,Options=Options).Create()
+        if 11 not in Devices:
+            Domoticz.Device(Name="Apparent_Power_L2", Unit=11,TypeName="Custom",Used=0,Options=Options).Create()
+        if 12 not in Devices:
+            Domoticz.Device(Name="Apparent_Power_L3", Unit=12,TypeName="Custom",Used=0,Options=Options).Create()
+        Options = { "Custom" : "1;VAr"}
+        if 13 not in Devices:
+            Domoticz.Device(Name="Reactive_Power_L1", Unit=13,TypeName="Custom",Used=0,Options=Options).Create()
+        if 14 not in Devices:
+            Domoticz.Device(Name="Reactive_Power_L2", Unit=14,TypeName="Custom",Used=0,Options=Options).Create()
+        if 15 not in Devices:
+            Domoticz.Device(Name="Reactive_Power_L3", Unit=15,TypeName="Custom",Used=0,Options=Options).Create()
+        Options = { "Custom" : "1;PF"}
+        if 16 not in Devices:
+            Domoticz.Device(Name="Power_Factor_L1", Unit=16,TypeName="Custom",Used=0,Options=Options).Create()
+        if 17 not in Devices:
+            Domoticz.Device(Name="Power_Factor_L2", Unit=17,TypeName="Custom",Used=0,Options=Options).Create()
+        if 18 not in Devices:
+            Domoticz.Device(Name="Power_Factor_L3", Unit=18,TypeName="Custom",Used=0,Options=Options).Create()
+        Options = { "Custom" : "1;°"}
+        if 19 not in Devices:
+            Domoticz.Device(Name="Phase_Angle_L1", Unit=19,TypeName="Custom",Used=0,Options=Options).Create()
+        if 20 not in Devices:
+            Domoticz.Device(Name="Phase_Angle_L2", Unit=20,TypeName="Custom",Used=0,Options=Options).Create()
+        if 21 not in Devices:
+            Domoticz.Device(Name="Phase_Angle_L3", Unit=21,TypeName="Custom",Used=0,Options=Options).Create()
+        if 22 not in Devices:
+            Domoticz.Device(Name="Average_Voltage_To_Neutral", Unit=22,TypeName="Voltage",Used=0).Create()
+        Options = { "Custom" : "1;A"}
+        if 23 not in Devices:
+            Domoticz.Device(Name="Average_Line_Current", Unit=23,TypeName="Custom",Used=0,Options=Options).Create()
+        if 24 not in Devices:
+            Domoticz.Device(Name="Sum_Line_Current", Unit=24,TypeName="Custom",Used=0,Options=Options).Create()
+        if 25 not in Devices:
+            Domoticz.Device(Name="Total_System_Power", Unit=25,TypeName="Usage",Used=0).Create()
+        Options = { "Custom" : "1;VA"}
+        if 26 not in Devices:
+            Domoticz.Device(Name="Total_System_Apparent_Power", Unit=26,TypeName="Custom",Used=0,Options=Options).Create()
+
                
     def onStop(self):
-        Domoticz.Log("Eastron SDM120Modbus plugin stop")
+        Domoticz.Log("Eastron SDM630Modbus plugin stop")
 
     def onHeartbeat(self):
         self.runInterval -=1;
         if self.runInterval <= 0:
             # Get data from SDM72D
-            Total_System_Power = self.rs485.read_float(84, functioncode=4, numberOfRegisters=2)
-            Import_Wh = self.rs485.read_float(72, functioncode=4, numberOfRegisters=2)
-            Export_Wh = self.rs485.read_float(74, functioncode=4, numberOfRegisters=2)
-            Total_kwh = self.rs485.read_float(342, functioncode=4, numberOfRegisters=2)
-            Voltage = self.rs485.read_float(0, functioncode=4, numberOfRegisters=2)
-            Import_power = self.rs485.read_float(88, functioncode=4, numberOfRegisters=2)
-            Export_power = self.rs485.read_float(92, functioncode=4, numberOfRegisters=2)
+            Voltage_L1 = self.rs485.read_float(0, functioncode=4, numberOfRegisters=2)
+            Voltage_L2 = self.rs485.read_float(2, functioncode=4, numberOfRegisters=2)
+            Voltage_L3 = self.rs485.read_float(4, functioncode=4, numberOfRegisters=2)
+            Current_L1 = self.rs485.read_float(6, functioncode=4, numberOfRegisters=2)
+            Current_L2 = self.rs485.read_float(8, functioncode=4, numberOfRegisters=2)
+            Current_L3 = self.rs485.read_float(10, functioncode=4, numberOfRegisters=2)
+            Power_L1 = self.rs485.read_float(12, functioncode=4, numberOfRegisters=2)
+            Power_L2 = self.rs485.read_float(14, functioncode=4, numberOfRegisters=2)
+            Power_L3 = self.rs485.read_float(16, functioncode=4, numberOfRegisters=2)
+            Apparent_Power_L1 = self.rs485.read_float(18, functioncode=4, numberOfRegisters=2)
+            Apparent_Power_L2 = self.rs485.read_float(20, functioncode=4, numberOfRegisters=2)
+            Apparent_Power_L3 = self.rs485.read_float(22, functioncode=4, numberOfRegisters=2)
+            Reactive_Power_L1 = self.rs485.read_float(24, functioncode=4, numberOfRegisters=2)
+            Reactive_Power_L2 = self.rs485.read_float(26, functioncode=4, numberOfRegisters=2)
+            Reactive_Power_L3 = self.rs485.read_float(28, functioncode=4, numberOfRegisters=2)
+            Power_Factor_L1 = self.rs485.read_float(30, functioncode=4, numberOfRegisters=2)
+            Power_Factor_L2 = self.rs485.read_float(32, functioncode=4, numberOfRegisters=2)
+            Power_Factor_L3 = self.rs485.read_float(34, functioncode=4, numberOfRegisters=2)
+            Phase_Angle_L1 = self.rs485.read_float(36, functioncode=4, numberOfRegisters=2)
+            Phase_Angle_L2 = self.rs485.read_float(38, functioncode=4, numberOfRegisters=2)
+            Phase_Angle_L3 = self.rs485.read_float(40, functioncode=4, numberOfRegisters=2)
+            Average_Voltage_To_Neutral = self.rs485.read_float(42, functioncode=4, numberOfRegisters=2)
+            Average_Line_Current = self.rs485.read_float(46, functioncode=4, numberOfRegisters=2)
+            Sum_Line_Current = self.rs485.read_float(48, functioncode=4, numberOfRegisters=2)
+            Total_System_Power = self.rs485.read_float(52, functioncode=4, numberOfRegisters=2)
+            Total_System_Apparent_Power = self.rs485.read_float(56, functioncode=4, numberOfRegisters=2)
             
             #Update devices
-            Devices[1].Update(0,str(Total_System_Power))
-            Devices[2].Update(0,str(Import_Wh*1000))
-            Devices[3].Update(0,str(Export_Wh*1000))
-            Devices[4].Update(0,str(Total_kwh))
-            Devices[5].Update(0,str(Voltage))
-            Devices[6].Update(0,str(Import_power))
-            Devices[7].Update(0,str(Export_power))
+            Devices[1].Update(0,str(Voltage_L1))
+            Devices[2].Update(0,str(Voltage_L2))
+            Devices[3].Update(0,str(Voltage_L3))
+            Devices[4].Update(0,str(round(Current_L1, 3)))
+            Devices[5].Update(0,str(round(Current_L2, 3)))
+            Devices[6].Update(0,str(round(Current_L3, 3)))
+            Devices[7].Update(0,str(round(Power_L1, 3)))
+            Devices[8].Update(0,str(round(Power_L2, 3)))
+            Devices[9].Update(0,str(round(Power_L3, 3)))
+            Devices[10].Update(0,str(Apparent_Power_L1))
+            Devices[11].Update(0,str(Apparent_Power_L2))
+            Devices[12].Update(0,str(Apparent_Power_L3))
+            Devices[13].Update(0,str(Reactive_Power_L1))
+            Devices[14].Update(0,str(Reactive_Power_L2))
+            Devices[15].Update(0,str(Reactive_Power_L3))
+            Devices[16].Update(0,str(Power_Factor_L1))
+            Devices[17].Update(0,str(Power_Factor_L2))
+            Devices[18].Update(0,str(Power_Factor_L3))
+            Devices[19].Update(0,str(Phase_Angle_L1))
+            Devices[20].Update(0,str(Phase_Angle_L2))
+            Devices[21].Update(0,str(Phase_Angle_L3))
+            Devices[22].Update(0,str(Average_Voltage_To_Neutral))
+            Devices[23].Update(0,str(Average_Line_Current))
+            Devices[24].Update(0,str(Sum_Line_Current))
+            Devices[25].Update(0,str(Total_System_Power))
+            Devices[25].Update(0,str(Total_System_Apparent_Power))
             
             
             if Parameters["Mode6"] == 'Debug':
-                Domoticz.Log("Eastron SD120Modbus Data")
-                Domoticz.Log('Total system power: {0:.3f} W'.format(Total_System_Power))
-                Domoticz.Log('Import Wh: {0:.3f} kWh'.format(Import_Wh))
-                Domoticz.Log('Export Wh: {0:.3f} kWh'.format(Export_Wh))
-                Domoticz.Log('Total kwh: {0:.3f} kWh'.format(Total_kwh))
-                Domoticz.Log('Voltage: {0:.3f} V'.format(Voltage))
-                Domoticz.Log('Import power: {0:.3f} kWh'.format(Import_power))
-                Domoticz.Log('Export power: {0:.3f} kWh'.format(Export_power))
-               
+                Domoticz.Log("############ Eastron SD630Modbus Data #################")
+                Domoticz.Log('Voltage_L1: {0:.3f} V'.format(Voltage_L1))
+                Domoticz.Log('Voltage_L2: {0:.3f} V'.format(Voltage_L2))
+                Domoticz.Log('Voltage_L3: {0:.3f} V'.format(Voltage_L3))
+                Domoticz.Log('Current_L1: {0:.3f} A'.format(Current_L1))
+                Domoticz.Log('Current_L2: {0:.3f} A'.format(Current_L2))
+                Domoticz.Log('Current_L3: {0:.3f} A'.format(Current_L3))
+                Domoticz.Log('Power_L1: {0:.3f} W'.format(Power_L1))
+                Domoticz.Log('Power_L2: {0:.3f} W'.format(Power_L2))
+                Domoticz.Log('Power_L3: {0:.3f} W'.format(Power_L3))
+                Domoticz.Log('Apparent_Power_L1: {0:.3f} VA'.format(Apparent_Power_L1))
+                Domoticz.Log('Apparent_Power_L2: {0:.3f} VA'.format(Apparent_Power_L2))
+                Domoticz.Log('Apparent_Power_L3: {0:.3f} VA'.format(Apparent_Power_L3))
+                Domoticz.Log('Reactive_Power_L1: {0:.3f} VA'.format(Reactive_Power_L1))
+                Domoticz.Log('Reactive_Power_L2: {0:.3f} VA'.format(Reactive_Power_L2))
+                Domoticz.Log('Reactive_Power_L3: {0:.3f} VA'.format(Reactive_Power_L3))
+                Domoticz.Log('Power_Factor_L1: {0:.3f} PF'.format(Power_Factor_L1))
+                Domoticz.Log('Power_Factor_L2: {0:.3f} PF'.format(Power_Factor_L2))
+                Domoticz.Log('Power_Factor_L3: {0:.3f} PF'.format(Power_Factor_L3))
+                Domoticz.Log('Phase_Angle_L1: {0:.3f} °'.format(Phase_Angle_L1))
+                Domoticz.Log('Phase_Angle_L2: {0:.3f} °'.format(Phase_Angle_L2))
+                Domoticz.Log('Phase_Angle_L3: {0:.3f} °'.format(Phase_Angle_L3))
+                Domoticz.Log('Average_Voltage_To_Neutral: {0:.3f} V'.format(Average_Voltage_To_Neutral))
+                Domoticz.Log('Average_Line_Current: {0:.3f} A'.format(Average_Line_Current))
+                Domoticz.Log('Sum_Line_Current: {0:.3f} A'.format(Sum_Line_Current))
+                Domoticz.Log('Total_System_Power: {0:.3f} W'.format(Total_System_Power))
+                Domoticz.Log('Total_System_Apparent_Power: {0:.3f} VA'.format(Total_System_Apparent_Power))
             self.runInterval = int(Parameters["Mode3"]) * 6
 
 
